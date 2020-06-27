2f18d27ab9b2e8fb07dc8850e3b6188f5483f6ce
Liz Shulman <corgipower@hotmail.com>
2020-06-27 14:59:05 -0400

    everything except styles done, fighting with npm

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react'
+
+function Character(character) {
+
+    return (
+        <div>
+            <p>{character.name}<br />
+            Year of birth: {character.birthYear}</p>
+        </div>
+
+    )
+}
+
+export default Character;
